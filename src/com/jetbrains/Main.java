e2b308a30cea6ce7a43343a5773cb5d7919947ac
andres030300 <43822019+andres030300@users.noreply.github.com>
2020-02-29 20:47:02 -0600

    prueba

---
+++ src/com/jetbrains/Main.java
@@ -0,0 +1,11 @@
+package com.jetbrains;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int v;
+        int f;
+        //probando git
+	// write your code here
+    }
+}
