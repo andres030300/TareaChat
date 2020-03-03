46444384ac0438a4d32f2579e9668e9d7d21ea8c
andres030300 <43822019+andres030300@users.noreply.github.com>
2020-03-02 22:21:10 -0600

    Creación de clase para la interfaz en Client.java

--- src/com/tec/tarea_datos/Client.java
+++ src/com/tec/tarea_datos/Client.java
@@ -1,3 +1,37 @@
 package com.tec.tarea_datos;
 
+import javax.swing.*;
+import java.awt.*;
+import java.net.Socket;
 
+
+public class Client {
+
+    public static void main (String[] args){
+        Ventana ventana = new Ventana();
+
+    }
+
+}
+
+class Ventana extends JFrame {
+    public Ventana(){
+
+        setBounds(500,500,500,500);
+
+        JPanel panel= new JPanel();
+
+        panel.setLayout(new BorderLayout());
+
+        JTextField texto = new JTextField();
+
+        panel.add(texto, BorderLayout.SOUTH);
+
+        add(panel);
+
+        setVisible(true);
+
+    }
+
+
+}
--- src/com/tec/tarea_datos/Server.java
+++ src/com/tec/tarea_datos/Server.java
@@ -1,1 +1,30 @@
 package com.tec.tarea_datos;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.*;
+import java.io.*;
+import javax.swing.*;
+
+public class Server {
+    Socket socket = new Socket();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main (String[] args){
+
+    }
+
+}
